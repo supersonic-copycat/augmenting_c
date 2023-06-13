a9a89f4a673650840d3827b39940c350f75f44bb
William Blake <you@example.com>
2023-06-13 22:37:17 +0300

    copy code up to ch 3
    Now add few variables (yes, global for simplicity) and routines to
    manage it. Now we include math.h to use sin and cos, so we need to tell
    CMake link our binary against libm.
    
    If you run this program, you should see little square in gnuplot. Don't
    forget to kill old gnuplot instances (`$pkill gnuplot` should work)!
    Ready to add Guile support

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -5,6 +6,10 @@
 static FILE *global_output = NULL;
 static const int WIDTH = 10;
 static const int HEIGHT = 10;
+
+static double x, y;
+static double direction;
+static int pendown;
 
 static FILE *start_gnuplot() {
   FILE *output;
@@ -33,8 +38,53 @@ static FILE *start_gnuplot() {
   fflush(output);
   return output;
 }
+static void draw_line(FILE *output, double x1, double y1, double x2,
+                      double y2) {
+  fprintf(output, "plot [0:1] %f + %f * t, %f + %f * t notitle\n", x1, x2 - x1,
+          y1, y2 - y1);
+  fflush(output);
+}
+
+static void tortoise_reset() {
+  x = y = 0.0;
+  direction = 0.0;
+  pendown = 1;
+
+  fprintf(global_output, "clear\n");
+  fflush(global_output);
+}
+
+static void tortoise_pendown() { pendown = 1; }
+
+static void tortoise_penup() { pendown = 0; }
+
+static void tortoise_turn(double degrees) {
+  direction += M_PI / 180.0 * degrees;
+}
+
+static void tortoise_move(double length) {
+  double newX, newY;
+
+  newX = x + length * cos(direction);
+  newY = y + length * sin(direction);
+
+  if (pendown)
+    draw_line(global_output, x, y, newX, newY);
+
+  x = newX;
+  y = newY;
+}
 
 int main(int argc, char **argv, char **envp) {
   global_output = start_gnuplot();
+  tortoise_reset();
+  {
+    int i;
+    tortoise_pendown(); /* This is unnecessary, but makes it clearer.  */
+    for (i = 1; i <= 4; ++i) {
+      tortoise_move(3.0);
+      tortoise_turn(90.0);
+    }
+  }
   return EXIT_SUCCESS;
 }
