31be9348cc6f88d359dadad5b672b38b916aa39a
William Blake <you@example.com>
2023-06-17 15:01:22 +0300

    connect guile functions with C ones
    now tutorial is completed and out tortoise is fully controlled from
    Guile! changes are pretty obvious -- change our tortoise functions to
    take and return "Guiled"-values.

--- main.c
+++ main.c
@@ -47,37 +47,58 @@ static void draw_line(FILE *output, double x1, double y1, double x2,
   fflush(output);
 }
 
-static void tortoise_reset() {
+static SCM tortoise_reset() {
   x = y = 0.0;
   direction = 0.0;
   pendown = 1;
 
   fprintf(global_output, "clear\n");
   fflush(global_output);
+  return SCM_UNSPECIFIED;
 }
 
-static void tortoise_pendown() { pendown = 1; }
-
-static void tortoise_penup() { pendown = 0; }
-
-static void tortoise_turn(double degrees) {
-  direction += M_PI / 180.0 * degrees;
+static SCM tortoise_pendown() {
+  SCM result = scm_from_bool(pendown);
+  pendown = 1;
+  return result;
 }
 
-static void tortoise_move(double length) {
+static SCM tortoise_penup() {
+  SCM result = scm_from_bool(pendown);
+  pendown = 0;
+  return result;
+}
+
+static SCM tortoise_turn(SCM degrees) {
+  const double value = scm_to_double(degrees);
+  direction += M_PI / 180.0 * value;
+  return scm_from_double(direction * 180.0 / M_PI);
+}
+
+static SCM tortoise_move(SCM length) {
+  const double value = scm_to_double(length);
   double newX, newY;
 
-  newX = x + length * cos(direction);
-  newY = y + length * sin(direction);
+  newX = x + value * cos(direction);
+  newY = y + value * sin(direction);
 
   if (pendown)
     draw_line(global_output, x, y, newX, newY);
 
   x = newX;
   y = newY;
+
+  return scm_list_2(scm_from_double(x), scm_from_double(y));
 }
 
-static void *register_functions(void *data) { return NULL; }
+static void *register_functions(void *data) {
+  scm_c_define_gsubr("tortoise-reset", 0, 0, 0, &tortoise_reset);
+  scm_c_define_gsubr("tortoise-penup", 0, 0, 0, &tortoise_penup);
+  scm_c_define_gsubr("tortoise-pendown", 0, 0, 0, &tortoise_pendown);
+  scm_c_define_gsubr("tortoise-turn", 1, 0, 0, &tortoise_turn);
+  scm_c_define_gsubr("tortoise-move", 1, 0, 0, &tortoise_move);
+  return NULL;
+}
 
 int main(int argc, char **argv, char **envp) {
   global_output = start_gnuplot();
